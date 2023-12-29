6af861022532ab5f614d9ae161a2c11e8d0979e2
Miroslava <145531749+miroslavakalman@users.noreply.github.com>
2023-12-29 14:39:37 +0300

    Add files via upload

---
+++ HTTP/script.js
@@ -0,0 +1,30 @@
+//222
+//1
+// let paramsString = 'test1=param1&test2=param2&test3=param3';
+
+// paramsString += '&test4=param4';
+
+// console.log(paramsString);
+
+//2
+// let paramsString = 'test1=param1&test2=param2&test3=param3';
+// let paramsArray = paramsString.split('&');
+// paramsArray = paramsArray.filter(param => !param.startsWith('test2='));
+// paramsString = paramsArray.join('&');
+
+// console.log(paramsString);
+
+//3
+// let paramsString = 'test1=param1&test2=param2&test3=param3';
+
+// let paramsArray = paramsString.split('&');
+
+// let test1Index = paramsArray.findIndex(param => param.startsWith('test1='));
+
+// if (test1Index !== -1) {
+//     paramsArray[test1Index] = 'test1=newValue';
+// }
+
+// paramsString = paramsArray.join('&');
+
+// console.log(paramsString);
